c7c06e0386b57d655240d2e594357bba9eb15dce
vilimos <134429538+sevelater@users.noreply.github.com>
2023-06-08 09:46:32 +0000

    html

---
+++ app/Example.tsx
@@ -0,0 +1,18 @@
+export const Example = () => {
+  return (
+    <div>
+    <header>
+    <title>prohi-bufe</title>
+    </header>
+    
+    <nav> 
+    <a href="#kinalat">Kínálat</a>
+    <a href="#rendeles">Rendelés</a>
+    <a href="#megszokottak">Megszokottak</a>
+    
+    </nav>
+    
+  
+    </div> 
+  )
+}
